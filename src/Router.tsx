202d0f45443f0a0ad5494f0b453657dd2cad21b0
MasatoYaguchi <ygcmst0510@gmail.com>
2022-04-12 19:30:16 +0900

    vite.config.tsのbaseを絶対パスに変更

--- src/Router.tsx
+++ src/Router.tsx
@@ -5,10 +5,11 @@ import HistoryView from "./HistoryView";
 
 
 function Router(): JSX.Element {
-    // const baseName = import.meta.env.BASE_URL;
-    const baseName = "";
+    const baseName = import.meta.env.BASE_URL;
     return (
         <BrowserRouter basename={baseName}>
+            {console.log(baseName)}
+            {console.log(import.meta.env.MODE)}
             <Routes>
                 <Route path="/" element={<InputBodyWeight />} />
                 <Route path="history" element={<HistoryView />} />
--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: 'https://masatoyaguchi.github.io/ReactTest',
   build: {
     outDir: 'docs'
   }
